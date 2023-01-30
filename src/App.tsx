811c80b084115852b1539d9002f61347c6db3b66
muchisx <mnm.soul@gmail.com>
2023-01-30 00:09:33 -0500

    set up scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import './styles/main.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <h1 className='test-title'>
+        Test Title
+        <span> im a span</span>
+      </h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
