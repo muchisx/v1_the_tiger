99d4b2c532181b175eec889fdfd90615ee384143
muchisx <mnm.soul@gmail.com>
2023-05-12 21:47:16 -0500

    fix filenamechange

--- web/src/pages/content/Home/index.tsx
+++ web/src/pages/content/Home/index.tsx
@@ -1,4 +1,4 @@
 export { default as HeroHeadingText } from './HeroHeadingText';
-export { default as homeCTA } from './homeCTA';
+export { default as homeCTA } from './homeCTAs';
 export { default as homeAboutMe } from './homeAboutMe';
 export { default as heroImage } from './homeImageBanner';
