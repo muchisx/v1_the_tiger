5fd010c61f07140fbbbc55e2d86c2d4a2d1cdaad
muchisx <mnm.soul@gmail.com>
2023-05-18 22:50:43 -0500

    update homeLogoGrid logos

--- web/src/pages/content/Home/homeLogoGrid.tsx
+++ web/src/pages/content/Home/homeLogoGrid.tsx
@@ -1,11 +1,13 @@
 import shopifyPartnerSecondary from '../../../assets/svgs/ShopifyPartner_Secondary.svg';
 import shopifyPlusPrimary from '../../../assets/svgs/ShopifyPlus_Primary.svg';
+import reactLogoBW from '../../../assets/svgs/react-logo-bw.svg';
+import typescriptLogoBW from '../../../assets/svgs/typescript-logo-neutral.svg';
 
 const logoUrlList = [
   shopifyPartnerSecondary,
   shopifyPlusPrimary,
-  shopifyPartnerSecondary,
-  shopifyPlusPrimary,
+  reactLogoBW,
+  typescriptLogoBW,
   shopifyPartnerSecondary,
   shopifyPlusPrimary,
 ];
