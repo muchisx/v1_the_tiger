0b7563213889919e3083d73235186cf41f09acbf
muchisx <mnm.soul@gmail.com>
2023-06-01 17:42:32 -0500

    Split content component improvements

--- web/src/components/SplitContent/SplitContent.tsx
+++ web/src/components/SplitContent/SplitContent.tsx
@@ -23,8 +23,16 @@ function SplitContent(props: Props) {
   const { topButton, leftTexts, backgroundShapeURL, buttonsLabel, leftButtons, maskedImageURL, leftHeading } =
     leftContent;
   const { rightButtons, rightTexts, cards } = rightContent;
+
+  // Validation - Section Props - checks if contain is passed in before passing down containType
   const containProps = contain ? { contain, containType } : { contain };
 
+  // Validation - Parent Renders - checks if children exist before rendering parents
+  const renderLeftContent =
+    topButton || leftTexts || backgroundShapeURL || buttonsLabel || leftButtons || maskedImageURL || leftHeading;
+  const renderRightContent = rightButtons || rightTexts || cards;
+
+  // Animations - Scroll Progress variables - checks the scroll progress of the page and assigns its value in a variable
   const { scrollYProgress } = useScroll();
   const shapeRotation = useTransform(scrollYProgress, [0, 1], [180, 192]);
 
@@ -40,49 +48,54 @@ function SplitContent(props: Props) {
         <BackgroundShape src={backgroundShapeURL} style={{ rotateZ: shapeRotation }} loading="lazy" />
       )}
 
-      <SubSection className="split-content__first">
-        {topButton && <Button {...topButton} />}
+      {renderLeftContent && (
+        <SubSection className="split-content__first">
+          {topButton && <Button {...topButton} />}
 
-        {leftHeading && <Heading {...leftHeading} />}
+          {leftHeading && <Heading {...leftHeading} />}
 
-        {leftTexts?.length && leftTexts.map((textProps) => <Text key={textProps.id} {...textProps} />)}
+          {leftTexts?.length && leftTexts.map((textProps) => <Text key={textProps.id} {...textProps} />)}
 
-        {leftButtons?.length && (
-          <ButtonsContainer>
-            {buttonsLabel && <ButtonsHeading>{buttonsLabel}</ButtonsHeading>}
-            {leftButtons.map((buttonProps) => (
-              <Button key={buttonProps.id} {...buttonProps} />
-            ))}
-          </ButtonsContainer>
-        )}
+          {leftButtons?.length && (
+            <ButtonsContainer>
+              {buttonsLabel && <ButtonsHeading>{buttonsLabel}</ButtonsHeading>}
+              {leftButtons.map((buttonProps) => (
+                <Button key={buttonProps.id} {...buttonProps} />
+              ))}
+            </ButtonsContainer>
+          )}
 
-        {maskedImageURL && <ImageMasked height={144} width={144} src={maskedImageURL} css={ImageMaskedCSS} />}
-      </SubSection>
-      <SubSection className="split-content__second">
-        {rightTexts?.length && rightTexts.map((textProps) => <Text key={textProps.id} {...textProps} />)}
+          {maskedImageURL && <ImageMasked height={144} width={144} src={maskedImageURL} css={ImageMaskedCSS} />}
+        </SubSection>
+      )}
 
-        {cards?.length && (
-          <CardsContainer>
-            {cards.map((cardProps, index) => (
-              <Card
-                key={cardProps.id}
-                {...cardProps}
-                customStyles={css`
-                  --order: ${index + 1};
-                `}
-              />
-            ))}
-          </CardsContainer>
-        )}
+      {renderRightContent && (
+        <SubSection className="split-content__second">
+          {rightTexts?.length && rightTexts.map((textProps) => <Text key={textProps.id} {...textProps} />)}
 
-        {rightButtons?.length && (
-          <ButtonsContainer>
-            {rightButtons.map((buttonProps) => (
-              <Button key={buttonProps.id} {...buttonProps} />
-            ))}
-          </ButtonsContainer>
-        )}
-      </SubSection>
+          {cards?.length && (
+            <CardsContainer>
+              {cards.map((cardProps, index) => (
+                <Card
+                  key={cardProps.id}
+                  {...cardProps}
+                  customStyles={css`
+                    --order: ${index + 1};
+                  `}
+                />
+              ))}
+            </CardsContainer>
+          )}
+
+          {rightButtons?.length && (
+            <ButtonsContainer>
+              {rightButtons.map((buttonProps) => (
+                <Button key={buttonProps.id} {...buttonProps} />
+              ))}
+            </ButtonsContainer>
+          )}
+        </SubSection>
+      )}
     </Section>
   );
 }
