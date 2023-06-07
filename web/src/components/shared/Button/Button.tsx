78cdd87ef7a65a4855d11b6401d4ab3fcf33d710
muchisx <mnm.soul@gmail.com>
2023-06-06 23:43:37 -0500

    organize better Button Component

--- web/src/components/shared/Button/Button.tsx
+++ web/src/components/shared/Button/Button.tsx
@@ -17,11 +17,19 @@ function Button(props: Props) {
     rel = 'noopener noreferrer',
   } = props;
 
+  // 1️⃣ Target and Rel setters for when button is a 'link'
+  // -------------------------- --------------------------
+  const target = newTab ? '_blank' : '_self';
+  const relValue = newTab ? rel : undefined;
+  // -------------------------- --------------------------
+
+  // 2️⃣ Animation handler for when both text and icon are present
+  // -------------------------- --------------------------
   const [isHovered, setIsHovered] = useState<boolean>(false);
   const isHoveredRef = useRef<boolean>(false);
   const timeoutRef = useRef<number>();
 
-  const handleHover = () => {
+  const handleTextAndIconHover = () => {
     // A more complex handler is needed for the hover animation so it doesn't break
     // The animation needs a delay on activation if the hover is too fast, it shouldn't happen
     isHoveredRef.current = !isHoveredRef.current;
@@ -30,10 +38,19 @@ function Button(props: Props) {
       setIsHovered(isHoveredRef.current);
     }, 50);
   };
+  // -------------------------- --------------------------
 
-  const target = newTab ? '_blank' : '_self';
-  const relValue = newTab ? rel : undefined;
+  // 3️⃣ Animation handler for when there's only one child (Text or Icon), but not both
+  // -------------------------- --------------------------
+  const singleChildAnimation = (buttonIcon?: Props['Icon'], buttonText?: Props['text']) => {
+    const animation =
+      buttonIcon && buttonText ? undefined : { scale: 1.1, transition: { ease: 'circIn', duration: 0.4 } };
+    return animation;
+  };
+  // -------------------------- --------------------------
 
+  // 4️⃣ Reusable Button Content
+  // -------------------------- --------------------------
   const buttonContent = (
     <>
       {text && <SpanStyled layout>{text}</SpanStyled>}
@@ -44,12 +61,7 @@ function Button(props: Props) {
       )}
     </>
   );
-
-  const hoverAnimation = (buttonIcon?: Props['Icon'], buttonText?: Props['text']) => {
-    const animation =
-      buttonIcon && buttonText ? undefined : { scale: 1.1, transition: { ease: 'circIn', duration: 0.4 } };
-    return animation;
-  };
+  // -------------------------- --------------------------
 
   return (
     <Container
@@ -57,9 +69,9 @@ function Button(props: Props) {
       text={text}
       Icon={Icon}
       IconWrapPadding={IconWrapPadding}
-      onMouseEnter={handleHover}
-      onMouseLeave={handleHover}
-      whileHover={hoverAnimation(Icon, text)}
+      onMouseEnter={handleTextAndIconHover}
+      onMouseLeave={handleTextAndIconHover}
+      whileHover={singleChildAnimation(Icon, text)}
       onClick={action}
       className={className}
       isHovered={isHovered}
