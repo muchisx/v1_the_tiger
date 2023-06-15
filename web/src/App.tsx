9fdf821dd24563f6835e1db38c10803d195b070d
muchisx <mnm.soul@gmail.com>
2023-06-14 22:51:29 -0500

    routes refactor

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,19 +2,10 @@
 import './styles/global-variables.css';
 import './styles/App.css';
 // Dependencies
-import { Route, Routes } from 'react-router-dom';
-// Pages
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
-import Playground from './pages/Playground';
+import { Outlet } from 'react-router-dom';
+// Components
 import Header from './components/Header/Header';
-import About from './pages/About';
-import Work from './pages/Work';
 import Footer from './components/Footer/Footer';
-import Services from './pages/Services';
-import WebDev from './pages/WebDev';
-import Shopify from './pages/Shopify';
 // Contexts
 import MediaQueryProvider from './context/MediaQueryContext';
 
@@ -22,17 +13,8 @@ function App() {
   return (
     <MediaQueryProvider>
       <Header />
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/playground" element={<Playground />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/web-dev" element={<WebDev />} />
-        <Route path="/shopify" element={<Shopify />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      {/* // Routes come from main.tsx */}
+      <Outlet />
       <Footer />
     </MediaQueryProvider>
   );
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+import Playground from './pages/Playground';
+import About from './pages/About';
+import Work from './pages/Work';
+import Services from './pages/Services';
+import WebDev from './pages/WebDev';
+import Shopify from './pages/Shopify';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="playground" element={<Playground />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="work" element={<Work />} />
+          <Route path="services" element={<Services />} />
+          <Route path="web-dev" element={<WebDev />} />
+          <Route path="shopify" element={<Shopify />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
