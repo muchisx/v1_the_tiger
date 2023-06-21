4aac2b5f40ad3969ecbc13dd8808e38120d3b809
muchisx <mnm.soul@gmail.com>
2023-06-20 23:50:21 -0500

    img banner types update

--- web/src/components/ImageBanner/imageBanner.types.tsx
+++ web/src/components/ImageBanner/imageBanner.types.tsx
@@ -1,4 +1,13 @@
+import type { Height } from '../../types/css.types';
+
 export type Props = {
   src: string;
+  height?: Height;
+  imgOverlayColor?: string;
   enableParallax?: boolean;
+};
+
+export type ContainerProps = {
+  $height?: Height;
+  $imgOverlayColor?: Props['imgOverlayColor'];
 };
