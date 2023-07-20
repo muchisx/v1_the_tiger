0fcf00d70cf4f81b063dfaeb5b4f31efbd4fc556
muchisx <mnm.soul@gmail.com>
2023-07-19 23:52:02 -0500

    small edit to playground

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,11 +1,11 @@
-import { ArrowRight } from '@styled-icons/fluentui-system-filled';
+import { Tent } from '@styled-icons/fluentui-system-filled';
 import Button from '../components/shared/Button/Button';
 import ComponentIsolator from '../components/ComponentIsolator/ComponentIsolator';
 
 function Playground() {
   return (
     <ComponentIsolator>
-      <Button role="link" variant="primary" to="www.google.com" text="hey" Icon={ArrowRight} />
+      <Button role="link" variant="primary" to="/" text="hey, i'm working on this" Icon={Tent} />
     </ComponentIsolator>
   );
 }
