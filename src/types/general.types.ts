7d389a0261ba9ea77bee7c546398791de2cff763
muchisx <mnm.soul@gmail.com>
2023-09-16 19:45:16 -0500

    add more App css and css var types

--- src/types/general.types.ts
+++ src/types/general.types.ts
@@ -7,3 +7,15 @@ export type NeutralColorCSSVar =
   | 'var(--neutral-color-quaternary)';
 
 export type AvailableCSSFontVar = 'var(--text-family)' | 'var(--heading-family)';
+
+export type TextColorCSSVar =
+  | 'var(--text-primary)'
+  | 'var(--text-secondary)'
+  | 'var(--text-tertiary)'
+  | 'var(--body-text-primary)'
+  | 'var(--body-text-secondary)'
+  | 'var(--body-text-tertiary)'
+  | 'var(--text-highlight-primary)'
+  | 'var(--text-highlight-secondary)'
+  | 'var(--text-highlight-tertiary)'
+  | 'var(--text-highlight-quaternary)';
