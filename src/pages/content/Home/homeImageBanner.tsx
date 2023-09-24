52888845425113b37643c71eb0e3f73429082064
muchisx <mnm.soul@gmail.com>
2023-09-23 19:34:16 -0500

    update hero banner

--- src/pages/content/Home/homeImageBanner.tsx
+++ src/pages/content/Home/homeImageBanner.tsx
@@ -1,7 +1,7 @@
 // Dependencies
 import { css } from 'styled-components';
 // Assets
-import heroImage from '@assets/images/hero-banner-2.png';
+import heroImage from '@assets/images/hero-banner-3.webp';
 // Types
 import { type Props as imageBannerProps } from '@components/ImageBanner/imageBanner.types';
 
