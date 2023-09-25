ba7c042cc542a78288b03735c1e1fbabd6c038c6
muchisx <mnm.soul@gmail.com>
2023-09-25 12:47:59 -0500

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+// Dependencies
+import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 // Styles
 import './styles/global-variables.css';
 import './styles/App.css';
-// Dependencies
-import { Outlet, ScrollRestoration } from 'react-router-dom';
 // Components
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
@@ -12,6 +13,7 @@ import MediaQueryProvider from '@context/MediaQueryContext';
 function App() {
   return (
     <MediaQueryProvider>
+      <Analytics />
       <ScrollRestoration />
       <Header />
       {/* // Routes come from main.tsx */}
